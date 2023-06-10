d2cc281e097b732066f9d019e995c21aae4bdfec
M6a5x98 <130889212+M6a5x98@users.noreply.github.com>
2023-06-10 18:16:10 +0200

    login files
    Html login page

---
+++ login/login.js
@@ -0,0 +1,14 @@
+let requeteUsername = new XMLHttpRequest();
+let requetePassword = new XMLHttpRequest();
+requeteUsername.open("POST", "url");
+requetePassword.open("POST", "url");
+let uname;
+let pword;
+
+document.querySelector("form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  uname = e.target[0].value;
+  pword = e.target[1].value;
+  /*requeteUsername.send(uname)
+    requetePassword.send(pword)*/
+});
