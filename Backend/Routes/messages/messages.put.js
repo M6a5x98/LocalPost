0d3c2139d4dd26473ce5998950991eae0e82aab2
M6a5x98 <130889212+M6a5x98@users.noreply.github.com>
2024-02-28 14:24:22 +0100

    Update PUT /api/messages

--- Backend/Routes/messages/messages.put.js
+++ Backend/Routes/messages/messages.put.js
@@ -5,6 +5,9 @@ const {
   TestTokenHeader,
   GetValueFromAccountFile,
   TokenExists,
+  ReadAccountFile,
+  GetPostProperties,
+  Post2HTML,
 } = require("../utils");
 const { appendFileSync, readFileSync, writeFileSync } = require("fs");
 
@@ -18,7 +21,7 @@ function like(req, res) {
     res.status(400);
     res.setHeader("Content-Type", "text/html");
     res.send(
-      `<h1>Error</h1><strong>Bonjour, s'il vous plaît arretez de faire joujou avec mon API,</strong><br /><p>vous saviez que si j'avais pas mis un try catch,</p><br /><p>le serveur aurait crash (ça rime hihihi)</p><br /><span><a href="https://github.com/m6a5x98" target="_blank">m6a5x98</a> le créateur de l'API vous remercie de prendre compte au + vite de ce message et de mettre les headers qu'il faut</span><footer><p>For pepole who come from USA or from other countries I didn't do the translation so use <a href="https://translate.google.com/">the best tool</a> !</p></footer>`
+      `<h1>Error</h1><strong>Bonjour, s'il vous plaît arretez de faire joujou avec mon API,</strong><br /><p>vous saviez que si j'avais pas mis un try catch,</p><br /><p>le serveur aurait crash (ça rime hihihi)</p><br /><span><a href="https://github.com/m6a5x98" target="_blank">m6a5x98</a> le créateur de l'API vous remercie de prendre compte au + vite de ce message et de mettre les trucs qu'il faut (headers, params, ... )</span><footer><p>For pepole who come from USA or from other countries I didn't do the translation so use <a href="https://translate.google.com/">the best tool</a> !</p></footer>`
     );
     console.warn(
       `Request without localpost-token header sended from ${req.ip}`
@@ -46,8 +49,21 @@ function like(req, res) {
   const postID = req.params["id"];
   if (CanLike(postID, GetValueFromAccountFile(Token(token), ["id"]))) {
     appendFileSync(
-      "/Fichiers.txt/Code/LocalPost/FrontAndBack/Backend/assets/likes.prop",
-      `${postID}=${GetValueFromAccountFile(Token(token), ["id"])},${reactCode}`
+      "../../assets/likes.prop",
+      `${postID}=${GetValueFromAccountFile(Token(token), [
+        "id",
+      ])},${reactCode}\n`
+    );
+    const userFile = ReadAccountFile(Token(token));
+    const UpdatedLikes = GetValueFromAccountFile(Token(token), ["likes"]);
+    UpdatedLikes.push(parseInt(postID));
+    Object.defineProperty(userFile, "likes", {
+      value: UpdatedLikes,
+    });
+
+    writeFileSync(
+      "../../assets/Users/" + Token(token) + ".json",
+      JSON.stringify(userFile)
     );
     res.json({ succes: true });
   }
@@ -63,7 +79,7 @@ function edit(req, res) {
     res.status(400);
     res.setHeader("Content-Type", "text/html");
     res.send(
-      `<h1>Error</h1><strong>Bonjour, s'il vous plaît arretez de faire joujou avec mon API,</strong><br /><p>vous saviez que si j'avais pas mis un try catch,</p><br /><p>le serveur aurait crash (ça rime hihihi)</p><br /><span><a href="https://github.com/m6a5x98" target="_blank">m6a5x98</a> le créateur de l'API vous remercie de prendre compte au + vite de ce message et de mettre les headers qu'il faut</span><footer><p>For pepole who come from USA or from other countries I didn't do the translation so use <a href="https://translate.google.com/">the best tool</a> !</p></footer>`
+      `<h1>Error</h1><strong>Bonjour, s'il vous plaît arretez de faire joujou avec mon API,</strong><br /><p>vous saviez que si j'avais pas mis un try catch,</p><br /><p>le serveur aurait crash (ça rime hihihi)</p><br /><span><a href="https://github.com/m6a5x98" target="_blank">m6a5x98</a> le créateur de l'API vous remercie de prendre compte au + vite de ce message et de mettre les trucs qu'il faut (headers, params, ... )</span><footer><p>For pepole who come from USA or from other countries I didn't do the translation so use <a href="https://translate.google.com/">the best tool</a> !</p></footer>`
     );
     console.warn(
       `Request without localpost-token header sended from ${req.ip}`
@@ -86,9 +102,7 @@ function edit(req, res) {
     GetValueFromAccountFile(Token(token), ["id"]) ===
     JSON.parse(
       readFileSync(
-        "/Fichiers.txt/Code/LocalPost/FrontAndBack/Backend/assets/message/" +
-          req.params["id"] +
-          ".json",
+        "../../assets/message/" + req.params["id"] + ".json",
         "utf-8"
       )
     )["data"]["author"]["id"];
@@ -99,9 +113,7 @@ function edit(req, res) {
   if (ActionPerformedByCreator || EditerIsAdminOrModo) {
     const NewPostFile = JSON.parse(
       readFileSync(
-        "/Fichiers.txt/Code/LocalPost/FrontAndBack/Backend/assets/message/" +
-          req.params["id"] +
-          ".json",
+        "../../assets/message/" + req.params["id"] + ".json",
         "utf-8"
       )
     );
@@ -111,12 +123,11 @@ function edit(req, res) {
     });
 
     writeFileSync(
-      "/Fichiers.txt/Code/LocalPost/FrontAndBack/Backend/assets/message/" +
-        req.params["id"] +
-        ".json",
+      "../../assets/message/" + req.params["id"] + ".json",
       JSON.stringify(NewPostFile)
     );
-    res.json({ succes: true });
+
+    res.json({ succes: true, html: Post2HTML(GetPostProperties(NewPostFile)) });
   } else {
     res.status(401);
     res.json({
