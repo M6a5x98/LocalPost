0696fdc6d9d7949522f28bec6e67fa9dfc776ba4
M6a5x98 <130889212+M6a5x98@users.noreply.github.com>
2024-03-02 20:07:58 +0100

    Update messages.delete.js
    Add like deleting in user file when a post is deleted

--- Backend/Routes/messages/messages.delete.js
+++ Backend/Routes/messages/messages.delete.js
@@ -6,10 +6,11 @@ const {
   ReadAccountFile,
   TokenExists,
 } = require("../utils");
-const { readFileSync, writeFileSync } = require("fs");
+const { readFileSync, writeFileSync, readdir } = require("fs");
+const { extname } = require("path");
 
 function del(req, res) {
-  //Pour verifier si le token est présent si oui l'assigner à la varoable du même nom sinon on envoie un code 400 au client.
+  //Pour verifier si le token est présent si oui l'assigner à la variable du même nom sinon on envoie un code 400 au client.
   let token;
   try {
     TestTokenHeader(req.headers["localpost-token"]);
@@ -17,7 +18,7 @@ function del(req, res) {
     res.status(400);
     res.setHeader("Content-Type", "text/html");
     res.send(
-      `<h1>Error</h1><strong>Bonjour, s'il vous plaît arretez de faire joujou avec mon API,</strong><br /><p>vous saviez que si j'avais pas mis un try catch,</p><br /><p>le serveur aurait crash (ça rime hihihi)</p><br /><span><a href="https://github.com/m6a5x98" target="_blank">m6a5x98</a> le créateur de l'API vous remercie de prendre compte au + vite de ce message et de mettre les headers qu'il faut</span><footer><p>For pepole who come from USA or from other countries I didn't do the translation so use <a href="https://translate.google.com/">the best tool</a> !</p></footer>`
+      `<h1>Error</h1><strong>Bonjour, s'il vous plaît arretez de faire joujou avec mon API,</strong><br /><p>vous saviez que si j'avais pas mis un try catch,</p><br /><p>le serveur aurait crash (ça rime hihihi)</p><br /><span><a href="https://github.com/m6a5x98" target="_blank">m6a5x98</a> le créateur de l'API vous remercie de prendre compte au + vite de ce message et de mettre les trucs qu'il faut (headers, params, ... )</span><footer><p>For pepole who come from USA or from other countries I didn't do the translation so use <a href="https://translate.google.com/">the best tool</a> !</p></footer>`
     );
     console.warn(
       `Request without localpost-token header sended from ${req.ip}`
@@ -37,9 +38,7 @@ function del(req, res) {
     GetValueFromAccountFile(Token(token), ["id"]) ===
     JSON.parse(
       readFileSync(
-        "../../assets/message/" +
-          parseInt(req.params["id"]) +
-          ".json",
+        "./assets/message/" + parseInt(req.params["id"]) + ".json",
         "utf-8"
       )
     )["data"]["author"]["id"];
@@ -57,11 +56,51 @@ function del(req, res) {
       const builtFile = ReadAccountFile(Token(token));
       Object.defineProperty(builtFile, "messages", { value: messages });
       writeFileSync(
-        "../../assets/Users/" +
-          Token(token) +
-          ".json",
+        "./assets/Users/" + Token(token) + ".json",
         JSON.stringify(builtFile)
       );
+      //Supprimer tous les likes du fichier
+      let rebuiltLikeFile = "";
+      readFileSync("./assets/likes.prop", "utf-8")
+        .split("\n")
+        .filter((l) => !l.startsWith(req.params["id"]))
+        .forEach((post) => (rebuiltLikeFile += post + "\n"));
+      writeFileSync("./assets/likes.prop", rebuiltLikeFile);
+      //supprimer le post de tous les fichiers utilisateurs
+      readdir("./assets/Users/", (err, file) => {
+        if (err) throw new Error(err);
+        //Get les fichiers JSON des likers
+        let fileNames = file.filter(
+          (files) => extname(files).toLowerCase() === ".json"
+        );
+        let parsedFiles = [];
+        fileNames.forEach((jsonFile) =>
+          parsedFiles.push(
+            JSON.parse(readFileSync("./assets/Users/" + jsonFile, "utf-8"))
+          )
+        );
+        const likers = parsedFiles["filter"]((uFile) =>
+          uFile.likes.includes(parseInt(req.params["id"]))
+        );
+        //Get les likers
+        for (let I = 0; I < likers.length; I++) {
+          const liker = likers[I];
+          Object.defineProperty(liker, "likes", {
+            value: liker["likes"]["filter"](
+              (likes) => likes !== parseInt(req.params["id"])
+            ),
+          });
+          likers[I] = liker;
+        }
+        for (let I = 0; I < likers.length; I++) {
+          const uFile = likers[I];
+          const uName = likers[I]["username"];
+          writeFileSync(
+            "./assets/Users/" + uName + ".json",
+            JSON.stringify(uFile)
+          );
+        }
+      });
       //Envoyer la requête su succès
       res.json({ succes: true });
     } else {
